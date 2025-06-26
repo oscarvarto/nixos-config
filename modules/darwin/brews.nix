d9a527a046b9ae727e6f2f74e8b20297993a1963
Oscar Vargas Torres <contact@oscarvarto.mx>
2025-06-25 21:30:03 -0600

    Split homebrew and window manager configurations into separate files

---
+++ modules/darwin/brews.nix
@@ -0,0 +1,115 @@
+{ config, pkgs, lib, ... }:
+
+{
+  homebrew = {
+    enable = true;
+    onActivation = {
+      autoUpdate = true;
+      cleanup = "none";
+      extraFlags = [ "--verbose" ];
+      upgrade = true;
+    };
+
+    taps = [
+      "d12frosted/homebrew-emacs-plus"
+    ];
+
+    brews = [
+      # Essential brews
+      "fish"
+      "7-zip"
+      "atuin"
+      "autoconf"
+      "autoconf-archive"
+      "automake"
+      "awscli"
+      "bat"
+      "bat-extras"
+      "bfg"
+      "carapace"
+      "cargo-binstall"
+      "ccache"
+      "cmake"
+      "coursier"
+      "difftastic"
+      {
+        name = "emacs-plus@31";
+        args = [ "with-xwidgets" "with-imagemagick" "with-savchenkovaleriy-big-sur-curvy-3d-icon" "with-mailutils" ];
+        link = true;
+      }
+      "eza"
+      "fish-lsp"
+      "ffmpeg"
+      "gradle"
+      "git-filter-repo"
+      "gnuplot"
+      "go"
+      "helix"
+      "hugo"
+      "imagemagick"
+      "isync"
+      "jq"
+      "libedit"
+      "libvterm"
+      "libsql"
+      "libtool"
+      "llvm"
+      "lua"
+      "markdown-oxide"
+      "marksman"
+      "maven"
+      "mise"
+      "mosh"
+      "mu"
+      "mysql@8.4"
+      "nasm"
+      "ncurses"
+      "ninja"
+      "nushell"
+      "pandoc"
+      "pueue"
+      "pass"
+      "pkg-config"
+      "pinentry-mac"
+      "pixi"
+      "poppler"
+      "resvg"
+      "stow"
+      "swig"
+      "trash-cli"
+      "uv"
+      "vivid"
+      "vcpkg"
+      "volta"
+      "wmctrl"
+      "xz" # lzma is part of xz
+      "yq"
+      "zellij"
+      "zig"
+    ];
+
+    casks = pkgs.callPackage ./casks.nix {};
+    caskArgs.appdir = "/Applications";
+
+    # onActivation.cleanup = "uninstall";
+
+    # These app IDs are from using the mas CLI app
+    # mas = mac app store
+    # https://github.com/mas-cli/mas
+    #
+    # $ nix shell nixpkgs#mas
+    # $ mas search <app name>
+    #
+    # If you have previously added these apps to your Mac App Store profile (but not installed them on this system),
+    # you may receive an error message "Redownload Unavailable with This Apple ID".
+    # This message is safe to ignore. (https://github.com/dustinlyons/nixos-config/issues/83)
+
+    masApps = {
+      "1Password for Safari" = 1569813296;  # Temporarily disabled
+      "neptunes" = 1006739057;
+      "Okta Verify" = 490179405;
+      "rcmd" = 1596283165;
+      "XCode" = 497799835;
+    };
+  };
+}
